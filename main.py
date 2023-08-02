1b9c73c9c1ad402df85f9bf7ba73c50ff1ff5db8
Michael-Zapivahin <zapivahin@ya.ru>
2023-08-02 15:29:10 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-# This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+def load_books():
+    book = []
+    with open('questions/1vs1200.txt', "rb") as file:
+        data = file.read().decode('koi8-r')
+    is_it_question = False;
+    is_it_answer = False;
+    question, answer = '', ''
+    for line in data.splitlines():
+        if not line:
+            is_it_answer = False
+            is_it_question = False
+        if is_it_question:
+            question += f'{line.strip()}\n'
+        if is_it_answer:
+            answer += f'{line.strip()}\n'
+        if line.find('Вопрос') >= 0:
+            if question:
+                book.append({'question': question, 'answer': answer})
+            question = ''
+            is_it_question = True
+            is_it_answer = False
+            continue
+        if line.find('Ответ') >= 0:
+            answer = ''
+            is_it_question = False
+            is_it_answer = True
+            continue
+    print(book)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    load_books()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
